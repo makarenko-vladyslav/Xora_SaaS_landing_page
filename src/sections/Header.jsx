dd401df40a65e1b08d8d94f86a2f377cdcdf104f
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-09-25 17:31:21 +0200

    inital project

---
+++ src/sections/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <a href="/">
+                    <img src="/images/xora.svg" alt="Xora logo" width={155} height={55}/>
+                </a>
+            </div>
+        </header>
+    )
+}
+export default Header
